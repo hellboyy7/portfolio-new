4c5951fa2d0e4c356e1ab65a59627bbb67d0371e
Abhaya Shilpakar <shilpakar567@gmail.com>
2025-01-08 13:06:53 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className=" text-red-600">
+      <h1>hello world </h1>
+      <h2>hello world </h2>
+    </div>
+  );
+};
+
+export default App;
